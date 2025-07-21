a3e5596459a1ee7f0c1fd82803df534613d4f9c3
karinaaftrr <karinafitriamalia05@gmail.com>
2025-07-21 03:29:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const toggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+console.log("Portfolio Website Loaded");
 
